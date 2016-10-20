8f0bdd415a0630d7f60adb2d86a1d6d779deff14
Julián J. Méndez O <menoc.sk27@gmail.com>
2016-10-19 18:50:34 -0600

    Not GG

--- rt.c
+++ rt.c
@@ -22,9 +22,11 @@ static long double Xmin = -100;
 static long double Ymin = -100;
 
 //Others
-static long double Ia = 0.002;
+static long double Ia = 0.5;
 static long double e = 0.005;
 static time_t t;
+
+static int cont = 0;
 
 struct Color { 
 	long double r;
@@ -39,6 +41,9 @@ struct Vector{//Aqui podria ir un valor que sea de norma, por si es necesario us
 };
 
 struct Light{
+	long double Xp;
+	long double Yp;
+	long double Zp;	
 	long double c1;
 	long double c2;
 	long double c3;
@@ -50,25 +55,27 @@ struct Object {
 	long double Yc;
 	long double Zc;
 	long double otherData;
-	struct Intersection *(*intersectionFuncion)(struct Vector, struct Vector, struct Object);
+	//struct Intersection *(*intersectionFuncion)(struct Vector, struct Vector, struct Object);
 	long double Kd;
 	long double Ka;
 	struct Color color;
 };
 
+
 struct Intersection {
-	struct Vector *(*normalVector)();
+	//struct Vector *(*normalVector)();
 	long double Xi;
 	long double Yi;
 	long double Zi;
 	long double distance;
-	struct Object * object;
+	struct Object object;
 };
 
 static struct Light *Lights;
-static struct Object *Objects;
+static struct Object Objects[1];
 static struct Color BACKGROUND = {0.3, 0.3, 0.3};
-static struct Color Framebuffer[768][1366];
+static struct Color Framebuffer[1366][768];
+static struct Intersection tempIntersect;
 
 long double min(long double a, long double b){
     if(a < b) { return a; }
@@ -83,12 +90,12 @@ long double max(long double a, long double b){
 //////////////Vectors
 //DONE
 //Producto punto entre dos vectores
-long double pointProduct(struct Vector *a, struct Vector *	b){
+long double pointProduct(struct Vector a, struct Vector b){
 	long double pp = 0;
 
-	pp += (a->x * b->x);
-	pp += (a->y * b->y);
-	pp += (a->z * b->z);
+	pp += (a.x * b.x);
+	pp += (a.y * b.y);
+	pp += (a.z * b.z);
 	
 	return pp;
 }
@@ -107,20 +114,20 @@ struct Vector crossProduct(struct Vector a, struct Vector b){
 
 //DONE
 //Regresa la norma de un vector
-long double getNorm(struct Vector *vector){
-	long double norm = sqrt(pow(vector->x ,2) + pow(vector->y ,2) + pow(vector->z ,2));
+long double getNorm(struct Vector vector){
+	long double norm = sqrt(pow(vector.x ,2) + pow(vector.y ,2) + pow(vector.z ,2));
 	return norm;
 }
 
 //DONE
 //Regresa un vector normalizado
-struct Vector normalize(struct Vector *vector){
+struct Vector normalize(struct Vector vector){
 	long double norm = getNorm(vector);
 	struct Vector unitVector;
 
-	unitVector.x = vector->x / norm;
-	unitVector.y = vector->y / norm;
-	unitVector.z = vector->z / norm;
+	unitVector.x = vector.x / norm;
+	unitVector.y = vector.y / norm;
+	unitVector.z = vector.z / norm;
 
 	return unitVector;
 }
@@ -196,8 +203,7 @@ struct Color colorXintensity(long double I, struct Color color){
 
 //Interseccion con esferas
 //Pendiente
-struct Intersection * sphereIntersection(struct Vector anchor, struct Vector direction, struct Object object){
-	struct Intersection * intersection;
+struct Intersection *sphereIntersection(struct Vector anchor, struct Vector direction, struct Object object){
 	long double t, t1, t2;
 
 	long double Xdif = anchor.x - object.Xc;
@@ -205,15 +211,20 @@ struct Intersection * sphereIntersection(struct Vector anchor, struct Vector dir
 	long double Zdif = anchor.z - object.Zc;
 
 	long double B = 2 * ((direction.x * Xdif) + (direction.y * Ydif) + (direction.z * Zdif));
-	long double C = pow(Xdif, 2) + pow(Ydif, 2) + pow(Zdif, 2) + object.otherData;
+	long double C = (long double)pow(Xdif, 2) + pow(Ydif, 2) + pow(Zdif, 2) - pow(object.otherData,2);
 
 	long double discriminant = pow(B, 2) - (4 * C);
 
 	if(discriminant >= 0){
+		
+
 		long double root = sqrt(discriminant);
+
 		B *= -1;
-		t1 = (B + sqrt(discriminant))/2;
-		t2 = (B - sqrt(discriminant))/2;
+
+		t1 = (B + root)/2;
+		t2 = (B - root)/2;
+
 
 		if(t1 > e){
 			if(t2 > e){
@@ -228,19 +239,40 @@ struct Intersection * sphereIntersection(struct Vector anchor, struct Vector dir
 				return NULL;
 			}
 		}
+		
 
-		intersection->distance = t;
-		intersection->object = &object;
-		intersection->Xi = anchor.x + t * direction.x;
-		intersection->Yi = anchor.y + t * direction.y;
-		intersection->Zi = anchor.z + t * direction.z;
+		tempIntersect.distance = t;
+		printf("%LF\n", direction.y);
+
+		tempIntersect.object = object;
+
+		tempIntersect.Xi = anchor.x + t * direction.x;
+		tempIntersect.Yi = anchor.y + t * direction.y;
+		tempIntersect.Zi = anchor.z + t * direction.z;
+
+		printf("%LF %LF %LF \n", tempIntersect.Xi, tempIntersect.Yi, tempIntersect.Zi);
+		return &tempIntersect;
 	}else{
 		return NULL;
 	}
 }
 
+struct Vector sphereNormal(struct Object object, long double Xi, long double Yi, long double Zi) {
+	struct Vector normal;
+	
+
+	normal.x = Xi - object.Xc;
+	normal.y = Yi - object.Yc;
+	normal.z = Zi - object.Zc;
+
+	normal = normalize(normal);
+
+
+	return normal;
+}
+
 //Revisar
-struct Intersection * getFirstIntersection(struct Vector anchor, struct Vector direction){
+/*struct Intersection getFirstIntersection(struct Vector anchor, struct Vector direction){
 	struct Intersection * intersection;
 	struct Intersection * tempIntersection;
 	long double tmin;
@@ -251,54 +283,67 @@ struct Intersection * getFirstIntersection(struct Vector anchor, struct Vector d
 	int k;
 	int objectsAmount = sizeof(Objects)/sizeof(Objects[0]);
 	for(k = 0; k < objectsAmount; k++){
-		tempIntersection = Objects[k].intersectionFuncion (anchor, direction, Objects[k]);
+		tempIntersection = /*Objects[k]. sphereIntersection (anchor, direction, Objects[k]);
 		if(tempIntersection && tempIntersection->distance > e && tempIntersection->distance < tmin){
 			tmin = tempIntersection->distance;
 			intersection = tempIntersection;
 		}
 	}
 	return (intersection);
-}
+}*/
 
 //Pendiente
 //Funcion de que color del profe
 struct Color getColor(struct Vector anchor, struct Vector direction){
 	struct Color color;
-	struct Intersection * intersection;
-
-	intersection = getFirstIntersection(anchor, direction);
-
-	if(!intersection){
+	struct Intersection *tempIntersection;
+	struct Intersection intersection;
+	
+	tempIntersection = sphereIntersection(anchor, direction, Objects[0]);
+	
+	if(!tempIntersection){
 		color = BACKGROUND;
 	}else{
+		printf("heyoo");
+
+		intersection.Xi = tempIntersection->Xi;
+		intersection.Yi = tempIntersection->Yi;
+		intersection.Zi = tempIntersection->Zi;
+		intersection.distance = tempIntersection->distance;
+		intersection.object = tempIntersection->object;
+
 		int k;
 		int lightsAmount = sizeof(Lights)/sizeof(Lights[0]);
 		
-		struct Object *Q = intersection->object;
-		struct Vector *N = intersection->normalVector(); //normal unitaria a Q en punto (Xi, Yi, Zi) AQUI
+		struct Object Q = intersection.object;
+		struct Vector N = sphereNormal(intersection.object,intersection.Xi, intersection.Yi, intersection.Zi); 
 		
+
 		long double I = 0.0;
 		
 		long double Fatt;
-		struct Vector *L;
+		struct Vector L;
 		for(k = 0; k < lightsAmount; k++){
 			struct Vector intresectionPoint;
-			struct Intersection * obstacle = NULL;
+			//struct Intersection obstacle = NULL;
 			//obstacle = getFirstIntersection({intersection.Xi, intersection.Yi, intersection.Zi},L);
-			if(!obstacle){
-				//L = getUnitaryVector(k); AQUI
+			if(1 == 1){
+				struct Vector luz = {Lights[k].Xp-intersection.Xi, Lights[k].Yp-intersection.Yi, Lights[k].Zp-intersection.Zi};
+				L = normalize(luz);
+
+
 				long double pp = pointProduct(N, L);
 				long double distanceToLight = getNorm(L);
 				if(pp > 0.0){
 					Fatt = getAtunuationFactor(Lights[k], distanceToLight);
-					I = I + (pp * Q->Kd * Fatt * Lights[k].Ip);
+					I = I + (pp * Q.Kd * Fatt * Lights[k].Ip);
 				}
 			}
 		}
 
-		I = I + Ia * Q->Ka;
+		I = I + Ia * Q.Ka;
 		I = min(1.0, I);
-		color = colorXintensity(I, Q->color);
+		color = colorXintensity(I, Q.color);
 	}
 	return (color);
 }
@@ -320,17 +365,36 @@ int main(int argc, char *arcgv[]){
 	long double Xdif = Xmax - Xmin;
 	long double Ydif = Ymax - Ymin;
 
-	for (i = 0; i < Vres; i++){
-		for (j = 0; j < Hres; j++){
+
+	//Esfera ad hoc
+	struct Object esfera1;
+	esfera1.Xc = 0;
+	esfera1.Yc = 0;
+	esfera1.Zc = 30;
+	esfera1.otherData = 10;
+	
+
+	esfera1.Kd = 1;
+	esfera1.Ka = 1;
+
+	esfera1.color.r = 1;
+	esfera1.color.g = 0;	
+	esfera1.color.b = 0;
+
+	Objects[0] = esfera1;
+
+	for (i = 0; i < Hres; i++){
+		for (j = 0; j < Vres; j++){
 			Xw = (long double) ((i + (1/2)) * Xdif)/Hres + Xmin;
-			Yw = (long double) ((i + (1/2)) * Ydif)/Vres + Ymin;
+			Yw = (long double) ((j + (1/2)) * Ydif)/Vres + Ymin;
 
 			Xd = Xw - Xe;
 			Yd = Yw - Ye;
 			Zd = Zw - Ze;
 
-			L = sqrt(pow(Xd, 2) + pow(Yd, 2) + pow(Zd, 2));
 
+			L = sqrt(pow(Xd, 2) + pow(Yd, 2) + pow(Zd, 2));
+		
 			direction.x = Xd / L;
 			direction.y = Yd / L;
 			direction.z = Zd / L;
@@ -338,8 +402,11 @@ int main(int argc, char *arcgv[]){
 			color = getColor(anchor, direction);
 
 			Framebuffer[i][j] = color;
+
 		}
 	}
 	saveFile();
 }
+
+
 //////////////END Ray Tracer Stuff
